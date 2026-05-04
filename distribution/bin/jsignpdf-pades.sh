c7c331c94765f2383e5873e247d49a6bb75969dc
Josef Cacek <josef.cacek@gmail.com>
2026-05-04 10:39:12 +0200

    Release workflow (simplified the jsignpdf one) (#32)

--- distribution/bin/jsignpdf-pades.sh
+++ distribution/bin/jsignpdf-pades.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+set -euo pipefail
 
-DIRNAME=$(dirname "$(readlink -e "$0")")
-DIR=$(cd "$DIRNAME" || exit 112; pwd)
+# Follow symlinks until we get the real file.
+SOURCE="$0"
+while [ -L "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd -P)"
+  TARGET="$(readlink "$SOURCE")"
+  case "$TARGET" in
+    /*) SOURCE="$TARGET" ;;
+    *)  SOURCE="$DIR/$TARGET" ;;
+  esac
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd -P)"
 
-[ "$OSTYPE" = "cygwin" ] && DIR="$( cygpath -m "$DIR" )"
+[ "${OSTYPE:-}" = "cygwin" ] && DIR="$( cygpath -m "$DIR" )"
+
+JAVA_HOME="${JAVA_HOME-}"
+JAVA_OPTS="${JAVA_OPTS-}"
 
 JAVA=java
 if [ -n "$JAVA_HOME" ]; then
